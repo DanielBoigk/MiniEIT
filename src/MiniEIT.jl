30821a710fdd9a044f7fd9e6b29635c65d91cce3
DanielBoigk <71269893+DanielBoigk@users.noreply.github.com>
2026-08-10 15:09:57 +0200

    initial commit

---
+++ src/MiniEIT.jl
@@ -0,0 +1,11 @@
+module MiniEIT
+    using Ferrite, LinearAlgebra, SparseArrays
+    using Enzyme, Mooncake, Zygote, DifferentiationInterface
+    using IterativeSolvers, Krylov
+
+    include("MatrixAssembler.jl")
+
+
+
+
+end
